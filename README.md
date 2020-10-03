8c986f345b805a9978ed49ced3a53436179d4911
Marty Smith <marty.smith01@yahoo.com>
2020-10-02 21:26:39 -0400

    v1.0.0 fully tested and deployable

--- README.md
+++ README.md
@@ -1,3 +1,105 @@
 # Introduction
 
-This project is an assignment for my Thinkful Curriculum, Module 12.5: Asynchronous web apps / Simple GET requests.
+This project is an assignment for my Thinkful curriculum, *Module 12.5: Asynchronous web apps / Simple GET requests*.  The purpose is to practice making a successful GET request and using the returned data productively.
+
+The essential goals as enumerated in the assignment are:
+
+1. Create an app that lets users display up to 50 random dog images from the [Dog API](https://dog.ceo/dog-api/) and print the results to the console.
+2. Expand the app to display the returned images in the DOM.
+3. Create an additional app to allow users to retrieve a single image of a dog of a chosen breed.
+
+I chose to modify the assignment in a few ways to, in my mind, better follow the spirit of the assignment while achieving a more streamlined and visually appealing product.  I created one single app which accomplishes both core functions; the mechanisms to switch between functions is intuitive and doing so is very easy.  All results are printed to the console.
+
+The app utilizes a mobile-first design which is responsive to accomodate larger screens.
+
+No templates were provided as part of the assignment.  This product fully represents my own original work, with outside support and consultation detailed in the *Special Thanks* section.
+
+This app is also my first project to include a fully-detailed README in its initial commit.
+
+See *Appendix A: Assignment from Thinkful Module 12.5* for more information.
+
+# Version Notes
+
+The version of this app currently uploaded is 1.0.0.  I do not track updates to this README file if there were not associated changes to any other part of the app.
+
+* v1.0.0
+
+Initial commit.
+
+# Instructions for Use
+
+The page will initially load two buttons: *Mix it Up!* and *Let me choose!*.  The user clicks either button which will expose its respective input fields.
+
+If the user clicks *Mix it Up!*:
+
+1. A prompt will appear entering the user to enter a number from 1 to 50.
+2. Once the user has provided a valid input, a submission button reading *Show me the dogs!* will appear.  If the user deletes their input the submission button will disappear.
+3. When the user clicks *Show me the dogs!*, a results field will appear and display pictures of dogs in the number requested (maximum of 50).
+
+If the user clicks *Let me Choose!*:
+
+1. A drop-down menu will appear asking the user their favorite breed of dog. By default the first item in the list 'affenpinscher' is selected.
+2. After choosing a breed, the user clicks the *Show me the dogs!* button to submit the query.  A results field will appear and show a picture of the selected breed.
+
+Notes:
+
+* The user may click either of the *Mix it Up!* or *Let me choose!* buttons at any time to change which functionality they're in.  The page will re-render the input prompt as necessary.
+* The types of input fields were chosen to eliminate the possibility of invalid user inputs.  Even if the user inputs characters into the numerical search field the app will convert the string to an integer.
+* Details of the search query results can be viewed in the console.
+
+# Tech Framework
+
+This app uses:
+
+* HTML5 and CSS
+* JavaScript ES6
+* jQuery 3.4.1 slim
+
+I built this app using:
+
+* VSCode 1.49.2
+* Chrome 85 (for display and DevTools)
+* Postman v7.33.1
+* Git-Bash 2.28.0.1
+* GitHub (online portal, not desktop)
+* Windows 10 Pro v1909
+
+For an easy time reading the code files I recommend using a code viewer that wraps at no less than 160 characters.
+
+# API Documentation
+
+Complete documenation for the [Dog API](https://dog.ceo/dog-api/) can be found [here](https://dog.ceo/dog-api/documentation/).
+
+# Future Plans
+
+In the future I intend to publish this product to my portfolio.  Prior to doing so I would like to accomplish a few more things:
+
+* Clean up the code in general and possibly create nested functions to slim down the larger functions and reduce code duplications.
+* Alter the size of returned images so that the largest ones are not grossly large compared to the smallest ones.
+* Make the brown background for the results resize smaller when pictures don't occupy a full row.
+* Provide user feedback when the user requests > 50 pictures. The API's will not return more than 50 images even if more are requested.
+* Generate alt-text for images returned in the query to increase accessibility.
+
+# Contribute
+
+Although I'm always interested in meeting new collaborators I prefer to keep this an individual project as a skills demonstrator.
+
+# Special Thanks
+
+* My mentor with Thinkful, Vincent Ramdhanie, gave me ample assistance troubleshooting my code where I had issues.
+* Colors were found through use of the [Coolors Color Scheme Generator](https://coolors.co/).
+* Generating a mobile-first responsive web app was much easier using the [Multi-Screen Test Tool](http://whatismyscreenresolution.net/multi-screen-test) at [WhatIsMyScreenResolution](http://whatismyscreenresolution.net/).
+* The [Dog API](https://dog.ceo/dog-api/) provided a very nice set of pictures for this app/assignment.
+* I found the [Gorditas](https://fonts.google.com/specimen/Gorditas) font via Google Fonts.  It was created by Gustavo Dipre and Brenda Gallo and made available under the [Open Fonts License](https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL).
+* I found the [Open Sans](https://fonts.google.com/specimen/Open+Sans) font via Google Fonts.  It was created by [Steve Matteson](https://twitter.com/@SteveMatteson1) and made available under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0).
+* Jennifer and Natalie Smith (the programmer's wife and mother, respectively) provided moral support and beta testing.
+
+# Appendix A: Assignment from Thinkful Module 12.5
+
+1. Create an app that lets users choose to display between 1 and 50 random dog images, then prints the results to the console. The app should feature a form with a required input where users indicate the number of images to retrieve, and the input should default to `3`. Use the endpoint described in the "DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS COLLECTION" section of [this page of the DogAPI docs](https://dog.ceo/dog-api/documentation/random).
+
+2. Building on the previous app, create an app that lets users choose to display between 1 and 50 random dog images, then loads the images in the DOM. This app should adhere to all of the requirements from the first one, in addition to displaying the images in the DOM.
+
+3. Create an app that loads a single random image for a specific breed, based on a user input. This app should account for the happy case when the breed is found, as well as the unhappy case when it is not. Use the endpoint described in the "RANDOM IMAGE FROM A BREED COLLECTION" section of [this page of the DogAPI docs](https://dog.ceo/dog-api/documentation/breed). Note that the API will return an HTTP status code of 404 along with a JSON object with info about the error if a request is made for a breed that can't be found.
+
+The above text was copied from Thinkful's materials which are proprietary and being republished with permission in this README file.  All original formatting was preserved insofar as it can be reproduced with the technical limitations of a `.md` file.
--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-
-
 /*    Pseudocode
  *       1. Render page with header, form, and initial two buttons in form.
  *       2. Clicking a button reveals its input field; hides/clears other field.
@@ -15,162 +13,153 @@ let formFieldLabel = ''
 let formFieldId = '';
 let formFieldName = '';
 let formFieldPlaceholder = '';
-let formMaxLength = 50;
-let formMaxNum = 50;
-
-let numRequested = 0;
-
 
 /* ------ Template Generation Functions ------ */
 
-function genForm() {
-    console.log('3: gen form works');
+function genForm() {      /* generates the inital page form to prompt the user to choose either the 'Mix it Up' or 'Let Me Choose' function */
     return `<form id="get-dogs-form">
                 <div id="user-input-objs">
                     <div id="input-buttons">
-                        <input type="button" id='btn-mixitup' value="Mix it Up!"></input>             <!--- Button 1: "Mix it Up" --->
-                        <input type="button" id='btn-letmechoose' value="Let Me Choose!"></input>         <!--- Button 1: "Mix it Up" --->
+                        <input type="button" class='input-button' id='btn-mixitup' value="Mix it Up!"></input>             <!--- Button 1: "Mix it Up" --->
+                        <input type="button" class='input-button' id='btn-letmechoose' value="Let Me Choose!"></input>     <!--- Button 2: "Let Me Choose!" --->
                     </div>
-                    <div id="input-field">
+                    <div id="input-field">                          <!--- input field will render here --->
                     </div>
                 </div>
-                <div>
-                    <input type="submit" class='hide' id='btn-submit' value="Show me the dogs!">                         <!--- Submit button --->
-                </div>
+                <input type="submit" class='hide' id='btn-submit' value="Show me the dogs!">                         <!--- Submit button --->
             </form>
-            <section id="show-dogs-here">
+            <section id="show-dogs-here">                           <!--- results field will render here --->
             </section>`;
 }
 
-function genInputNumber() {
-    return  `<label for='${formFieldFor}'>${formFieldLabel}</label>
+function genInputNumber() {           /* generates the user input field for the 'Mix It Up' function */
+    return  `<label class='request-prompt' for='${formFieldFor}'>${formFieldLabel}</label>
              <input type="text" class="user-input-box" id='${formFieldId}' name='${formFieldName}' placeholder='${formFieldPlaceholder}'></input>`;
 }
 
-function genInputBreed() {
-    return  `<label for='${formFieldFor}'>${formFieldLabel}</label>
+function genInputBreed() {           /* generates the user input field for the 'Let Me Choose' function */
+    return  `<label class='request-prompt' for='${formFieldFor}'>${formFieldLabel}</label>
              <select class="user-input-box" id='${formFieldId}' name='${formFieldName}'>${breedsList}</select>`;
 }
 
-function genResults(responseJson) {
-    console.log('func genResults called');
-    let outputString = '';
-    if ($('#btn-mixitup').hasClass('active-path')) {
-        for(i=0; i<responseJson.message.length; i++) {
-            outputString += `<section id="show-dogs-here">
-                                <img src=${responseJson.message[i]}>
+function genResults(responseJson) {                                                        /* generates the results field */
+    let outputString = '';                                                                 /* creates an empty string to append results to */
+    if ($('#btn-mixitup').hasClass('active-path')) {                                       /* determines whether 'Mix it Up' is the active app functionality */
+        console.log(`User requested ${responseJson.message.length} dog pictures.`);              /* prints the query to the console */
+        console.log(`Results of user query follow.`);                                      
+        for(i=0; i<responseJson.message.length; i++) {                                           /* iterates a results item for the number of pictures requested */
+                                                                                                 /* template HTML container to hold a single image follows */
+            outputString += `<section id="show-dogs-here">                                     
+                                <img src=${responseJson.message[i]}>                   
                             </section>`;
+            console.log(`Dog picture ${i+1}: [${responseJson.message[i]}]`);                     /* prints the URL returned by the API to the console */
         }
-    } else {
+    } else {                                                                               /* defaults to 'Let Me Choose' functionality if not 'Mix it Up' */
+        console.log(`User requested a picture of a ${$('#input-breed').val()}.`);                /* prints the query to the console */
+        console.log(`Results of user query follow.`);                                 
+        console.log(`Picture: [${responseJson.message}]`);                                       /* prints the query result to the console */
+                                                                                                 /* template HTML container to hold a single image follows */
         outputString += `<section id="show-dogs-here">
                                 <img src=${responseJson.message}>
-                            </section>`;
+                         </section>`;
     }
-    return outputString;       
+    return outputString;                                                                    /* both app functionalities return results in the same variable */
 }
 
 /* ------ Render Functions ------ */
 
-function renderForm() {
-    console.log('2: render form works')
-    return $('main').html(genForm());
+function renderForm() {                                                 /* function to render the initial form of the page */
+    return $('main').html(genForm());                                   /* renders the HTML template string as functional HTML */
   }
 
-function renderInputNumber() {
-    return $('#input-field').html(genInputNumber(formFieldFor, formFieldLabel, formFieldId, formFieldName, formFieldPlaceholder));
+function renderInputNumber() {                                          /* function to render the form input specific to the 'Mix it Up' functionality */
+    return $('#input-field').html(genInputNumber());                    /* renders the HTML template string as functional HTML */
 }
 
-function renderInputBreed() {
-    return $('#input-field').html(genInputBreed(formFieldFor, formFieldLabel, formFieldId, formFieldName, formFieldPlaceholder));
+function renderInputBreed() {                                           /* function to render the form input specific to the 'Let Me Choose' functionality */
+    return $('#input-field').html(genInputBreed());                     /* renders the HTML template string as functional HTML */
 }
 
-function renderResults(responseJson) {
-    console.log('render results working');
-    return $('#show-dogs-here').html(genResults(responseJson))
+function renderResults(responseJson) {                                  /* function to render the results output, works for both functionalities */
+    return $('#show-dogs-here').html(genResults(responseJson))          /* renders the HTML template string as functional HTML */
 }
 
 /* ------ Event Handler Functions ------ */
 
-function searchType() {
-    $('main').on('click', '#btn-mixitup', function(event) {
-        $(this).addClass('active-path');
-        $(this).siblings().removeClass('active-path');
-        $('#get-dogs-form').find('#btn-submit').addClass('hide')
-        mixItUp();
+function searchType() {                                             /* this function listens to determine which functional pathway in the app the user takes */
+    $('main').on('click', '#btn-mixitup', function(event) {             /* listens for a click on the 'Mix it Up' button */
+        $(this).addClass('active-path');                                    /* marks 'Mix it Up' as the active pathway */
+        $(this).siblings().removeClass('active-path');                      /* unmarks 'Let Me Choose' as the active pathway (if applicable) */
+        $('#get-dogs-form').find('#btn-submit').addClass('hide')            /* hides the 'Let Me Choose' form input field (if applicable) */
+        mixItUp();                                                          /* initiates the 'Mix It Up' pathway */
+        $('#input-field').addClass('add-margin-15px');                      /* adds margin, this has to happen here or the page doesn't render properly */
     })
-    $('main').on('click', '#btn-letmechoose', function(event) {
-        $(this).addClass('active-path');
-        $(this).siblings().removeClass('active-path');
-        $('#get-dogs-form').find('#btn-submit').removeClass('hide')
-        letMeChoose();
+    $('main').on('click', '#btn-letmechoose', function(event) {         /* listens for a click on the 'Let Me Choose' button */
+        $(this).addClass('active-path');                                    /* marks 'Let Me Choose' as the active pathway */
+        $(this).siblings().removeClass('active-path');                      /* unmarks 'Mix it Up' as the active pathway (if applicable) */
+        $('#get-dogs-form').find('#btn-submit').removeClass('hide')         /* hides the 'Mix it Up' form input field (if applicable) */
+        letMeChoose();                                                      /* initiates the 'Mix It Up' pathway */
+        $('#input-field').addClass('add-margin-15px');                      /* adds margin, this has to happen here or the page doesn't render properly */
     })
 }
 
-function textInput() {
-    $('#get-dogs-form').on('keyup', '.user-input-box', function(event) {
-        if ($('.user-input-box').val() != "") {
-            $('#get-dogs-form').find('#btn-submit').removeClass('hide');
-            $('#get-dogs-form').find('#btn-submit').prop('disabled', false);
-        } else {
-            $('#get-dogs-form').find('#btn-submit').prop('disabled', true);
-            textInput();
+function textInput() {                                                        /* listens for user input in the 'Mix it Up' functionality */
+    $('#get-dogs-form').on('keyup', '.user-input-box', function(event) {          /* listens for the user to type a value into the text box */
+        if ($('.user-input-box').val() != "") {                                   /* determines if there is a value entered into the text box */
+            $('#get-dogs-form').find('#btn-submit').removeClass('hide');              /* reveals the form submit button */
+        } else {                                                                  /* determines if the user has deleted the contents of the text box */
+            $('#get-dogs-form').find('#btn-submit').addClass('hide');                 /* hides the tex box if there's no input */
+            textInput();                                                              /* re-initiates the function to wait for input */
         }
-    })
-}
+    })                                                                         //  Please note that the 'Let Me Choose' functionality is designed so that there
+}                                                                              //  is always a valid input, so it makes the form submit button visible by default
 
-function submitClick() {
-    $('#get-dogs-form').on('submit', function(event) {
-        event.preventDefault();
-        if ($('#btn-mixitup').hasClass('active-path')) {
-            getDogImageRandom();
-        } else {
-            getDogImageBreed();
+function submitClick() {                                                   
+    $('#get-dogs-form').on('submit', function(event) {           /* listens for user to click the form submit button */
+        event.preventDefault();                                  /* stops the page from sending the user input to the server */
+        $('#show-dogs-here').addClass('results-section');        /* adds CSS for the results section, this has to happen here or the page doesn't render properly */
+        if ($('#btn-mixitup').hasClass('active-path')) {         /* determines whether 'Mix it Up' is the active functionality */
+            getDogImageRandom();                                     /* triggers the API request form for the 'Mix it Up' field */
+        } else {                                                 /* if 'Mix it Up' is not the active functionality, assumes 'Let Me Choose' must be active */
+            getDogImageBreed();                                      /* triggers the API request form for the 'Let Me Choose' field */
         }
     })
 }
 
 /* ------ Structural Functions ------ */
 
-function mixItUp() {
+function mixItUp() {                                                 /* generates the proper attributes for the 'Mix it Up' input field */
     formFieldFor = 'input-quant';
     formFieldLabel = 'How many dogs do you want to see?'
     formFieldId = 'input-quant';
     formFieldName = 'quant';
-    formFieldPlaceholder = 'enter a 1 - 50 number, e.g.: 3';
-    renderInputNumber();
+    formFieldPlaceholder = 'enter 1-50, eg: 3';
+    renderInputNumber();                                             /* launches the render function for the 'Mix it Up' input field */
 }
 
-function letMeChoose() {
+function letMeChoose() {                                             /* generates the proper attributes for the 'Let Me Choose' input field */
     formFieldFor = 'input-breed';
     formFieldLabel = 'What is your favorite breed of dog?'
     formFieldId = 'input-breed';
     formFieldName = 'breed';
-    formFieldPlaceholder = 'e.g.: dachshund';
-    renderInputBreed();
+    renderInputBreed();                                              /* launches the render function for the 'Let Me Choose' input field */
 }
 
 /* ------ API Request Functions ------ */
 
-function getDogImageRandom() {
-    fetch(`https://dog.ceo/api/breeds/image/random/${$('#input-quant').val()}`)
-    .then(response => response.json())
-    .then(responseJson => renderResults(responseJson))
-        .catch(error => alert('Something went wrong. Try again later.'));
+function getDogImageRandom() {                                                       /* API request form for the 'Mix it Up' field */
+    fetch(`https://dog.ceo/api/breeds/image/random/${$('#input-quant').val()}`)          /* API endpoint when querying a number of random images */
+    .then(response => response.json())                                                   /* function waits for a response file from the API */
+    .then(responseJson => renderResults(responseJson))                                   /* launches a function to render results using the API's response */
+        .catch(error => alert('Something went wrong. Try again later.'));                /* catches invalid responses from the API and alerts the user */
 }
 
-function getDogImageBreed() {
-    fetch(`https://dog.ceo/api/breed/${$('#input-breed').val()}/images/random`)
-    .then(response => response.json())
-    .then(responseJson => renderResults(responseJson))
-        .catch(error => alert('Something went wrong. Try again later.'));
+function getDogImageBreed() {                                                        /* API request form for the 'Mix it Up' field */
+    fetch(`https://dog.ceo/api/breed/${$('#input-breed').val()}/images/random`)          /* API endpoint when querying a single breed-specific picture */
+    .then(response => response.json())                                                   /* function waits for a response file from the API */
+    .then(responseJson => renderResults(responseJson))                                   /* launches a function to render results using the API's response */
+        .catch(error => alert('Something went wrong. Try again later.'));                /* catches invalid responses from the API and alerts the user */
 }
-
-/*function getDogImageBreed() {
-    fetch()
-        .then()
-        .then()
-        .catch();
-}*/
 
 /* ------ Initialize Functions ------ */
 
@@ -179,7 +168,6 @@ function initializePage() {
     searchType();
     textInput();
     submitClick();
-    console.log("1: initializePage works")
 }
 
 $(initializePage());
